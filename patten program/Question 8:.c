f24a57a085d569fdb708866d2fb401faa211ef5f
DEEKAN17 <124654757+DEEKAN17@users.noreply.github.com>
2023-07-27 19:04:53 +0530

    Create Question 8:.c

---
+++ patten program/Question 8:.c
@@ -0,0 +1,40 @@
+/******************************************************************************
+
+                         hint: j
+    *                          1 2 3 4 5 6 7 8 9
+   * *                   i=1   _ _ _ _ * _ _ _ _   
+  * * *                  i=2   _ _ _ * _ * _ _ _
+ * * * *                 i=3   _ _ * _ * _ * _ _
+* * * * *                i=4   _ * _ * _ * _ * _  
+                         i=5   * _* _ * _ * _ * _
+                         
+    first loop for print row
+    second loop for print space (space -row)
+    third loop for print *_(_ means space)
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    int row,space,i,j;
+     printf("enter a row :");
+     scanf("%d",&row);
+     
+     
+     for(i=1;i<=row;i++)//row 
+     {
+         for(space=1;space<=(row-i);space++)//this for space 
+         {
+            printf(" ");
+         }
+         for(j=1;j<=i;j++)
+         {
+             printf("* ");
+         }
+         printf("\n");
+        
+     }
+     
+
+    return 0;
+}
