4545b2b17c98f38a70a3aa9d9c6f3aefad40bb30
DEEKAN17 <124654757+DEEKAN17@users.noreply.github.com>
2023-07-27 18:20:25 +0530

    Update Question 7:.c

--- patten program/Question 7:.c
+++ patten program/Question 7:.c
@@ -1,39 +1,27 @@
 /******************************************************************************
-
 1 
 2 3 
 3 4 5
 5 6 7 8
 
 *******************************************************************************/
-#include<stdio.h>
+
+#include <stdio.h>
 
 int main()
 {
-    int r,c,i,j,sum=0;
-    printf("enter a row :");
+int r,c,i,j,num=1;//i=rows,j=column
     scanf("%d",&r);
-    printf("enter a column :");
     scanf("%d",&c);
-    for(i=1;i<=r;i++)
-    {
-         for(j=0;j<i;j++)
-         {
-            if(j<i+1)
-            {
-                sum=i+1;
-                printf("%d",sum);
-            }
-            else
-            {
-              printf("%d",i);
-            }
-         }
-         sum=0;
-    }
-        
-           
-           
-}
     
-   
+    for(i=0;i<=r;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("%d",num);
+            num ++;
+        }
+        printf("\n");
+    }
+    return 0;
+}
