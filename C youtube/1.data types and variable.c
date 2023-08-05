424fcb8189e79063f1db2cd2a225e501e542b38e
DEEKAN17 <124654757+DEEKAN17@users.noreply.github.com>
2023-08-05 20:00:44 +0530

    Create 1.data types and variable.c

---
+++ C youtube/1.data types and variable.c
@@ -0,0 +1,58 @@
+/******************************************************************************
+Basic program:
+    1.%d - Interger
+    2.%f -float 
+    3.%C -character
+    4.%lf-long float
+    
+
+*******************************************************************************/
+//1. Interger:
+
+/*#include <stdio.h>
+
+int main()
+{
+    int a;
+    printf("Enter a number:");
+    scanf("%d",&a);
+    printf("%d",a);
+    return 0;
+}*/
+
+/*//2.%f -float :
+
+#include<stdio.h>
+void main()
+ {
+    float b=1.545444;
+    printf("%f",b);
+ }
+*/
+
+/*//3.%C -character:
+
+#include<stdio.h>
+void main()
+ {
+    char c='q';
+    printf("%c",c);
+ }
+ */
+//4.%lf-long float:
+
+#include<stdio.h>
+
+void main()
+{
+    double d=9.55555555;
+    printf("lf", d);
+}
+ 
+
+
+
+
+
+
+
