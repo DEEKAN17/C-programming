ee08609216a7d9cc61250e9170af96f0066bac08
DEEKAN17 <124654757+DEEKAN17@users.noreply.github.com>
2023-08-25 22:34:30 +0530

    Create Fibonacci Series in C without recursion.c

---
+++ practice code .c/Fibonacci Series in C without recursion.c
@@ -0,0 +1,27 @@
+/******************************************************************************
+
+                          Fibonacci Series in C without recursion
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int lastnumber,n1=0,n2=1,n3;
+    
+    printf("enter a last number");
+    scanf("%d",&lastnumber);
+    printf("%d %d ",n1,n2);
+    
+    for(int i=2;i<lastnumber;i++)
+    {
+        n3=n1+n2;
+        printf("%d ",n3);
+        n1=n2;
+        n2=n3;
+    }
+
+    return 0;
+}
+
